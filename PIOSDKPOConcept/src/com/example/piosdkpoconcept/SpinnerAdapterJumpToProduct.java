e75f8233da6a5a59152b418b5174b65cf8a1b5a5
Vlado Pajic <vlado@print.io>
2014-12-24 14:25:16 +0100

    Fix related to jumping to product.

--- PIOSDKPOConcept/src/com/example/piosdkpoconcept/SpinnerAdapterJumpToProduct.java
+++ PIOSDKPOConcept/src/com/example/piosdkpoconcept/SpinnerAdapterJumpToProduct.java
@@ -14,8 +14,10 @@ import android.widget.TextView;
 public class SpinnerAdapterJumpToProduct extends BaseAdapter implements SpinnerAdapter {
 
 	private class NameIdPair {
+
 		public String name;
 		public int id;
+
 		public NameIdPair(String name, int id) {
 			this.name = name;
 			this.id = id;
@@ -34,7 +36,7 @@ public class SpinnerAdapterJumpToProduct extends BaseAdapter implements SpinnerA
 		items.add(new NameIdPair("Acrylic Blocks", PublicConstants.ProductIds.ACRYLIC_BLOCKS));
 		items.add(new NameIdPair("Acrylic Prints", PublicConstants.ProductIds.ACRYLIC_PRINTS));
 		items.add(new NameIdPair("Beach Bags", PublicConstants.ProductIds.BEACH_BAGS));
-		items.add(new NameIdPair("Calendars", PublicConstants.ProductIds.CALENDARS));
+		// items.add(new NameIdPair("Calendars", PublicConstants.ProductIds.CALENDARS)); // NOT AVALIABLE!
 		items.add(new NameIdPair("Canvas Wraps", PublicConstants.ProductIds.CANVAS_WRAPS));
 		items.add(new NameIdPair("Coasters", PublicConstants.ProductIds.COASTERS));
 		items.add(new NameIdPair("Cube Decor", PublicConstants.ProductIds.CUBE_DECOR));
@@ -47,13 +49,13 @@ public class SpinnerAdapterJumpToProduct extends BaseAdapter implements SpinnerA
 		items.add(new NameIdPair("Magnetgram", PublicConstants.ProductIds.MAGNETGRAM));
 		items.add(new NameIdPair("Metal Magnets", PublicConstants.ProductIds.METAL_MAGNETS));
 		items.add(new NameIdPair("Metal Prints", PublicConstants.ProductIds.METAL_PRINTS));
-		items.add(new NameIdPair("Minibook Deluxe", PublicConstants.ProductIds.MINIBOOK_DELUXE));
+		// items.add(new NameIdPair("Minibook Deluxe", PublicConstants.ProductIds.MINIBOOK_DELUXE)); // NOT AVALIABLE!
 		items.add(new NameIdPair("Minibooks", PublicConstants.ProductIds.MINIBOOKS));
 		items.add(new NameIdPair("Mousepads", PublicConstants.ProductIds.MOUSEPADS));
 		items.add(new NameIdPair("Mugs", PublicConstants.ProductIds.MUGS));
 		items.add(new NameIdPair("Playing Cards", PublicConstants.ProductIds.PLAYING_CARDS));
 		items.add(new NameIdPair("Postcards", PublicConstants.ProductIds.POSTCARDS));
-		items.add(new NameIdPair("Posters", PublicConstants.ProductIds.POSTERS));
+		// items.add(new NameIdPair("Posters", PublicConstants.ProductIds.POSTERS)); // NOT AVALIABLE!
 		items.add(new NameIdPair("Prints", PublicConstants.ProductIds.PRINTS));
 		items.add(new NameIdPair("Shower Curtains", PublicConstants.ProductIds.SHOWER_CURTAINS));
 		items.add(new NameIdPair("Stickerbooks", PublicConstants.ProductIds.STICKERBOOKS));
@@ -63,7 +65,7 @@ public class SpinnerAdapterJumpToProduct extends BaseAdapter implements SpinnerA
 		items.add(new NameIdPair("Throw Pillows", PublicConstants.ProductIds.THROW_PILLOWS));
 		items.add(new NameIdPair("Tinybooks", PublicConstants.ProductIds.TINYBOOKS));
 		items.add(new NameIdPair("Wall Skin", PublicConstants.ProductIds.WALL_SKIN));
-		items.add(new NameIdPair("Wood Prints", PublicConstants.ProductIds.WOOD_PRINTS));
+		// items.add(new NameIdPair("Wood Prints", PublicConstants.ProductIds.WOOD_PRINTS)); // NOT AVALIABLE!
 		items.add(new NameIdPair("Woven Blankets", PublicConstants.ProductIds.WOVEN_BLANKETS));
 	}
 
@@ -83,6 +85,7 @@ public class SpinnerAdapterJumpToProduct extends BaseAdapter implements SpinnerA
 	}
 
 	private class ViewHolder {
+
 		private TextView textViewName;
 	}
 
