76027ef4b807517e44c3520c80c3f8dda1a3ab61
Bojan Radivojevic <bojan@print.io>
2014-12-27 03:51:38 +0100

    Added alternate Photosources set

--- PIOSDKPOConcept/src/com/example/piosdkpoconcept/MainActivity.java
+++ PIOSDKPOConcept/src/com/example/piosdkpoconcept/MainActivity.java
@@ -189,6 +189,14 @@ public class MainActivity extends Activity {
 		photoSourcesTest.add(PhotoSource.PHOTOBUCKET);
 		photoSourcesTest.add(PhotoSource.DROPBOX);
 		//photoSourcesTest.add(PhotoSource.PICASA);
+		//photoSourcesTest.add(PhotoSource.PRESELECTED);
+	}
+
+	private void addAlternatePhotoSources() {
+		// Only up to 6
+		photoSourcesTest.add(PhotoSource.PHONE);
+		photoSourcesTest.add(PhotoSource.INSTAGRAM);
+		photoSourcesTest.add(PhotoSource.PRESELECTED);
 	}
 
 	private void addAllSideMenuInfoButtons() {
@@ -341,6 +349,7 @@ public class MainActivity extends Activity {
 
 		if (photoSourcesTest.size() == 0) {
 			addDefaultPhotoSources();
+			//addAlternatePhotoSources();
 		}
 		if (photoSourcesTest.size() != 0) {
 			PIO.setPhotoSources(photoSourcesTest);
