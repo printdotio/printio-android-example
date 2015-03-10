7914df92bf6a5e9f5f3963388ca5e6b2fbb1839a
Vlado Pajic <vlado@print.io>
2015-03-10 16:54:44 +0100

    Adding new products DOG_BEDS, CANVAS_POSTERS.

--- PIOSDKPOConcept/src/com/example/piosdkpoconcept/SpinnerAdapterJumpToProduct.java
+++ PIOSDKPOConcept/src/com/example/piosdkpoconcept/SpinnerAdapterJumpToProduct.java
@@ -64,6 +64,7 @@ public class SpinnerAdapterJumpToProduct extends BaseAdapter implements SpinnerA
 		items.add(new NameIdPair("Tablet Cases", PublicConstants.ProductIds.TABLET_CASES));
 		items.add(new NameIdPair("Thick Prints", PublicConstants.ProductIds.THICK_PRINTS));
 		items.add(new NameIdPair("Throw Pillows", PublicConstants.ProductIds.THROW_PILLOWS));
+		items.add(new NameIdPair("Dog Beds", PublicConstants.ProductIds.DOG_BEDS));
 		items.add(new NameIdPair("Tinybooks", PublicConstants.ProductIds.TINYBOOKS));
 		items.add(new NameIdPair("Wall Skin", PublicConstants.ProductIds.WALL_SKIN));
 		// items.add(new NameIdPair("Wood Prints", PublicConstants.ProductIds.WOOD_PRINTS)); // NOT AVALIABLE!
