4127b5bf5208566b8b75d0b6ff7543572c1ca07a
Vlado Pajic <vlado@print.io>
2015-03-26 20:45:45 +0100

    Updated "Vlado" photo source.

--- PIOSDKPOConcept/src/com/example/piosdkpoconcept/photosource/vlado/VladoPhotoSourceNavigator.java
+++ PIOSDKPOConcept/src/com/example/piosdkpoconcept/photosource/vlado/VladoPhotoSourceNavigator.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import print.io.photosource.defaultgenericimpl.DefaultPhotoSourceNavigator;
+import print.io.photosource.defaultgenericimpl.items.Album;
 import print.io.photosource.defaultgenericimpl.items.Folder;
 import print.io.photosource.defaultgenericimpl.items.Item;
 import print.io.photosource.defaultgenericimpl.items.Photo;
@@ -12,21 +13,53 @@ import android.support.v4.app.FragmentActivity;
 
 public class VladoPhotoSourceNavigator extends DefaultPhotoSourceNavigator<VladoPhotoSource> {
 
-	private List<String> IAMGE_URL = Arrays
+	private List<String> IMAGE_URL = Arrays
 			.asList("https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xfa1/v/t1.0-9/526189_4617697276507_662207867_n.jpg?oh=8d527dd9102f0124fe0833be2cb24c22&oe=5580D92A&__gda__=1434779874_f9059584437b839eb5149e3e4da2fc97",
 					"https://scontent-ams.xx.fbcdn.net/hphotos-prn2/t31.0-8/1268224_10201569069964657_1243815332_o.jpg");
+	private List<Album> albums = new ArrayList<Album>();
 
 	public VladoPhotoSourceNavigator(FragmentActivity activity, VladoPhotoSource photoSource, print.io.photosource.PhotoSourceNavigator.PhotoSourceNavigatorHolder holder) {
 		super(activity, photoSource, holder);
+		for (int i = 0; i < IMAGE_URL.size(); i++) {
+			albums.add(new VladoAlbum(IMAGE_URL.get(i), "Album " + (i + 1), 1, i));
+		}
 	}
 
 	@Override
 	protected void onLoadMedia(Folder folder, int startOffset) {
 		ArrayList<Item> photos = new ArrayList<Item>();
-		for (String imageUrl : IAMGE_URL) {
-			photos.add(new Photo(imageUrl, imageUrl));
+		if (folder == null) {
+			for (String imageUrl : IMAGE_URL) {
+				photos.add(new Photo(imageUrl, imageUrl));
+			}
+			for (Album album : albums) {
+				photos.add(album);
+			}
+		} else {
+			VladoAlbum album = (VladoAlbum) folder;
+			String img = IMAGE_URL.get(album.getIndex());
+			photos.add(new Photo(img, img));
 		}
 		loadMediaFinished(photos);
+	}
+
+	public class VladoAlbum extends Album {
+
+		private int index;
+
+		public VladoAlbum(String thumbnailUrl, String title, int photosCount, int index) {
+			super(thumbnailUrl, title, photosCount);
+			this.index = index;
+		}
+
+		public int getIndex() {
+			return index;
+		}
+
+		public void setIndex(int index) {
+			this.index = index;
+		}
+
 	}
 
 }
