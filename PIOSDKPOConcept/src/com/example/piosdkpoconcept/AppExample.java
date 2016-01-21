9aeadef9e5ddc50cd05975a5ee443fb27c6dae90
Vlado Pajic <vlado@print.io>
2016-01-21 15:41:15 +0100

    Removed obsolete comments

--- PIOSDKPOConcept/src/com/example/piosdkpoconcept/AppExample.java
+++ PIOSDKPOConcept/src/com/example/piosdkpoconcept/AppExample.java
@@ -5,20 +5,11 @@ import android.app.Application;
 import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
 
-//@ReportsCrashes(
-//		formKey = "",
-//		formUri = "http://bomberaya.iriscouch.com/acra-printio/_design/acra-storage/_update/report",
-//		reportType = org.acra.sender.HttpSender.Type.JSON,
-//		httpMethod = org.acra.sender.HttpSender.Method.PUT,
-//		formUriBasicAuthLogin="printsdk",
-//		formUriBasicAuthPassword="testpass876543"
-//		)
 public class AppExample extends Application {
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		//ACRA.init(this);
 		PIO.initializeParse(this, PIOConstants.Parse.APPLICATION_ID, PIOConstants.Parse.CLIENT_KEY);
 		LocalBroadcastManager.getInstance(this).registerReceiver(new PIOSDKEventListener(), new IntentFilter(PIOConstants.PIO_SDK_EVENTS));
 	}
