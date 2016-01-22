9bd1a6ade5459b9a430a853c19f6b35095eaf3b8
Vlado Pajic <vlado@print.io>
2016-01-22 16:31:26 +0100

    Parse has been removed from SDK

--- PIOSDKPOConcept/src/com/example/piosdkpoconcept/AppExample.java
+++ PIOSDKPOConcept/src/com/example/piosdkpoconcept/AppExample.java
@@ -1,6 +1,5 @@
 package com.example.piosdkpoconcept;
 
-import print.io.PIO;
 import android.app.Application;
 import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
@@ -10,7 +9,6 @@ public class AppExample extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		PIO.initializeParse(this, PIOConstants.Parse.APPLICATION_ID, PIOConstants.Parse.CLIENT_KEY);
 		LocalBroadcastManager.getInstance(this).registerReceiver(new PIOSDKEventListener(), new IntentFilter(PIOConstants.PIO_SDK_EVENTS));
 	}
 
